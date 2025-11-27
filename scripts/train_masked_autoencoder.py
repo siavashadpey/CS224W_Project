e502c4f95dc9d9f7867f11b23e39fade3035458d
ZGN\SS5867 <siavosh.shadpey@zebra.com>
2025-11-26 21:16:33 -0500

    removed debug code

--- scripts/train_masked_autoencoder.py
+++ scripts/train_masked_autoencoder.py
@@ -12,9 +12,8 @@ sys.path.append(str(project_root))
 from torch import nn, torch
 from torch_geometric import seed_everything
 from torch_geometric.data import Data as PyGData
-from torch_geometric.loader import DataLoader
+from torch_geometric.loader import create_gcs_dataloaders
 
-torch.autograd.set_detect_anomaly(True)
 
 seed_everything(1313)
 
@@ -45,25 +44,12 @@ def train_one_epoch(data_loader,
     total_loss = 0
     for batch in data_loader:
         batch = batch.to(device)
-        #print(batch)
         optimizer.zero_grad()
 
         batch_indices = batch.batch if hasattr(batch, 'batch') else torch.zeros(batch.x.size(0), dtype=torch.long, device=device)
         predicted_pos, mask_indices = model(batch.x, batch.pos, batch.edge_index, batch.edge_attr, batch_indices)
-        #print("pos_gt[mask]", batch.pos[mask_indices])
         loss = loss_fn(predicted_pos, batch.pos[mask_indices])
-        #print("loss=",loss.item())
         loss.backward()
-        #for name, param in model.named_parameters():
-        #    if param.grad is not None:
-        #        print(f"{name} grad min: {param.grad.min().item()}, max: {param.grad.max().item()}, mean: {param.grad.mean().item()}")
-        #        if torch.isnan(param.grad).any():
-        #            print(f"NaN detected in {name} gradients!")
-        #    else:
-        #        if param.requires_grad:
-        #            print(f"BUG? -> {name} requires a gradient but is None.")
-        #        print(f"{name} grad is None")
-        #input()
         optimizer.step()
         total_loss += loss.item()
     
@@ -94,16 +80,11 @@ def main():
     parser = argparse.ArgumentParser(description="Main script for the project.")
     
     # GCS arguments
-    """
     parser.add_argument('--gcs_bucket', type=str, required=True, help='GCS bucket name')
     parser.add_argument('--train_prefix', type=str, required=True, help='GCS prefix for training data')
     parser.add_argument('--val_prefix', type=str, required=True, help='GCS prefix for validation data')
     parser.add_argument('--test_prefix', type=str, required=True, help='GCS prefix for test data')
     parser.add_argument('--cache_dir', type=str, default='/tmp/pyg_cache', help='Local cache directory')
-    """
-    parser.add_argument('--num_workers', type=int, default=4, help='Number of data loading workers.')
-    parser.add_argument('--train_data_path', type=str, required=True, help='Local path to training data.')
-    parser.add_argument('--test_data_path', type=str, required=True, help='Local path to test data.')
 
     # Model save/load
     parser.add_argument('--model_save_path', type=str, required=True, help='Path to save the trained model.')
@@ -113,11 +94,12 @@ def main():
     parser.add_argument('--num_epochs', type=int, default=150, help='Number of training epochs.')
     parser.add_argument('--learning_rate', type=float, default=0.0001, help='Learning rate for the optimizer.')
     parser.add_argument('--batch_size', type=int, default=32, help='Batch size for training.')
+    parser.add_argument('--num_workers', type=int, default=4, help='Number of data loading workers.')
     
     # Model architecture
     parser.add_argument('--num_encoder_layers', type=int, default=3, help='Number of encoder layers in the model.')
     parser.add_argument('--num_decoder_layers', type=int, default=3, help='Number of decoder layers in the model.')
-    parser.add_argument('--hidden_dim', type=int, default=64, help='Hidden dimension size for the model.')
+    parser.add_argument('--hidden_dim', type=int, default=128, help='Hidden dimension size for the model.')
     parser.add_argument('--masking_ratio', type=float, default=0.45, help='Ratio of masking for input data.')
     
     # Checkpointing
@@ -141,7 +123,6 @@ def main():
 
     # Load training, testing, and validation data from GCS
     logger.info("Loading datasets from GCS...")
-    """
     train_loader, val_loader, test_loader, train_dataset = create_gcs_dataloaders(
         bucket_name=args.gcs_bucket,
         train_prefix=args.train_prefix,
@@ -151,24 +132,6 @@ def main():
         num_workers=args.num_workers,
         local_cache_dir=args.cache_dir,
         force_download=False
-    )
-    """
-
-    root_path = "/workspace/CS224W_Project/data/"
-    file_paths = ["/workspace/CS224W_Project/data/plgems_full_casf2016.pt"]
-    train_dataset = GCSPyGDataset(root=root_path, file_paths=file_paths)
-    print(train_dataset, len(train_dataset))
-    [print(train_dataset[0])]
-    #input()
-    #for graph in train_dataset:
-    #    if graph.id == '1bzc':
-    #        print(graph)
-    #input()
-    train_loader = DataLoader(
-        train_dataset, 
-        batch_size=args.batch_size, 
-        shuffle=True, 
-        num_workers=args.num_workers
     )
 
     # Model
@@ -191,8 +154,6 @@ def main():
              decoder=decoder,
              masking_ratio=args.masking_ratio)
     
-    print(model)
-    #input()
     for m in [encoder, decoder, model]:
         m.to(device)
     
@@ -218,15 +179,14 @@ def main():
                                      device)
 
         if epoch % args.checkpoint_interval == 0:
-            val_loss = 0 #eval(val_loader,
-                         #   model,
-                         #   l2_loss,
-                         #   device)
-            test_loss = 0 #eval(test_loader,
-                          #  model,
-                          #  l2_loss,
-                          #  device)
-
+            val_loss = eval(val_loader,
+                            model,
+                            l2_loss,
+                            device)
+            test_loss = eval(test_loader,
+                            model,
+                            l2_loss,
+                            device)
             if val_loss < best_val_loss:
                 best_val_loss = val_loss
                 best_model_state = model.state_dict()
